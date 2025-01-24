06871b12623326af2d372df2587c1aef717b6ee4
mlpeter33 <123034983+mlpeter33@users.noreply.github.com>
2025-01-24 12:10:21 -0600

    Added Icon

--- blockhubedu-landing/src/pages/index.js
+++ blockhubedu-landing/src/pages/index.js
@@ -1,16 +1,21 @@
 import Head from 'next/head';
 
-export default function Landing() {
+export default function Home() {
     return(
         <div>
             <Head>
                 <title>BlockHub Studio</title>
                 <meta name="description" content="BlockHub Edu is a platform for learning blockchain development." />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/Icon.png" />
             </Head>
-            <main>
-                <p className="text-pink-600">Welcome to BlockHub Studio</p>
-            </main>
+            <div className="relative h-screen overflow-hidden">
+                <div className="absolute top-0 left-0 w-full h-full bg-fixed bg-top" style={{ backgroundImage: "url('/assets/Background.png')" }}>
+                    
+                        <p className="flex justify-center items-center text-gray-800 text-4xl">Welcome to</p>
+                        <p className="flex justify-center items-center text-gray-800 text-5xl">BlockHub Studio</p>
+                </div>
+            </div>
         </div>
+
     )
 }
