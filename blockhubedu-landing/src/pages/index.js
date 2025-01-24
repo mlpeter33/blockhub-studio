4b127c8813d9a1c9b45e4b5afaf4ce24a370da9e
mlpeter33 <123034983+mlpeter33@users.noreply.github.com>
2025-01-24 16:05:52 -0600

    Update index.js

--- blockhubedu-landing/src/pages/index.js
+++ blockhubedu-landing/src/pages/index.js
@@ -2,11 +2,11 @@ export default function Home() {
         return(
         <div>
             <div className="relative h-screen overflow-hidden">
-                <div className="absolute top-0 left-0 w-full h-full bg-contain bg-scroll bg-top" style={{ backgroundImage: "url('/assets/Background.png')" }}>
+                <div className="absolute top-0 left-0 w-full h-full bg-contain bg-scroll bg-top flex flex-col justify-center items-center" style={{ backgroundImage: "url('/assets/Background.png')" }}>
                     
-                        <p className="flex justify-center items-center text-gray-800 text-4xl">Welcome to</p>
-                        <p className="flex justify-center items-center text-gray-800 text-7xl font-[cabinSketch] font-bold">BlockHub</p>
-                        <p className="flex justify-center items-center text-gray-800 text-7xl font-[cabinSketch] font-bold">Studio</p>
+                        <p className=" text-gray-800 text-4xl">Welcome to</p>
+                        <p className=" text-gray-800 text-7xl font-[cabinSketch] font-bold">BlockHub</p>
+                        <p className=" text-gray-800 text-7xl font-[cabinSketch] font-bold">Studio</p>
                 </div>
             </div>
         </div>
