4a8444fda4e215d53ae87108c957c308d9095985
mlpeter33 <mlacayopit@gmail.com>
2025-01-24 20:11:33 -0600

    button

--- blockhubedu-landing/src/pages/index.js
+++ blockhubedu-landing/src/pages/index.js
@@ -2,11 +2,11 @@ export default function Home() {
         return(
         <div>
             <div className="relative h-screen overflow-hidden">
-                <div className="absolute top-0 left-0 w-full h-full bg-contain bg-scroll bg-top flex flex-col justify-center items-center" style={{ backgroundImage: "url('/assets/Background.png')" }}>
+                <div className="absolute top-0 left-0 w-full h-full bg-scroll bg-top flex flex-col justify-center items-center" style={{ backgroundImage: "url('/assets/Background.png')" }}>
                     
-                        <p className=" text-gray-800 text-4xl">Welcome to</p>
-                        <p className=" text-gray-800 text-7xl font-[cabinSketch] font-bold">BlockHub</p>
-                        <p className=" text-gray-800 text-7xl font-[cabinSketch] font-bold">Studio</p>
+                        <p className=" text-gray-800 text-4xl mb-4 shadow-md">Welcome to</p>
+                        <p className=" text-gray-800 text-7xl font-[cabinSketch] font-bold mb-2 shadow-sm tracking-widest">BlockHub</p>
+                        <p className=" text-gray-800 text-7xl font-[cabinSketch] font-bold mb-2 shadow-md tracking-widest">Studio</p>
                 </div>
             </div>
         </div>
