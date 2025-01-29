e1b430649390cf601cf9e37e7842cba89ce9b52c
mlpeter33 <123034983+mlpeter33@users.noreply.github.com>
2025-01-29 14:00:39 -0600

    Improving header

--- blockhubedu-landing/src/components/header.tsx
+++ blockhubedu-landing/src/components/header.tsx
@@ -33,6 +33,30 @@ export default function Header() {
                </a>
                </div>
               </nav>
+              {/* Actions */}
+              <div className="flex space-x-6">
+              <button className="bg-transparent border-2 p-2  sketchy sketchy-none rounded-full hover:shadow-lg transition-transform duration-300 hover:scale-110 hover:bg-white">
+                  🌓
+              </button>
+              <button className="bg-transparent border-2 p-2  sketchy sketchy-none rounded-full hover:shadow-lg transition-transform duration-300 hover:scale-110 hover:bg-white">
+                  🚀
+              </button>
+              <div className="relative group">
+            <button className="rotate-180 flex w-10 h-10 bg-[url(/assets/Profile.png)] sketchy sketchy-none justify-center items-center bg-cover rounded-full hover:shadow-lg transition-transform duration-300 hover:scale-110 hover:rotate-0">
+            </button>
+            <div className="absolute right-0 mt-2 w-40 bg-white text-black rounded shadow-md hidden group-hover:block">
+              <a href="/profile" className="block px-4 py-2 hover:bg-gray-100">
+                Profile
+              </a>
+              <a href="/settings" className="block px-4 py-2 hover:bg-gray-100">
+                Settings
+              </a>
+              <a href="/logout" className="block px-4 py-2 hover:bg-gray-100">
+                Logout
+              </a>
+            </div>
+              </div>
+              </div>
         </div> <div className="text-white border-2 border-white sketchy"></div>
     </header>
   );
