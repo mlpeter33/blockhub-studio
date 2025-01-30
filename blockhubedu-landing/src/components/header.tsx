1667f80f784ce403ddae20aee1893743b140dcd9
mlpeter33 <123034983+mlpeter33@users.noreply.github.com>
2025-01-30 12:31:45 -0600

    Tweaking the header

--- blockhubedu-landing/src/components/header.tsx
+++ blockhubedu-landing/src/components/header.tsx
@@ -3,7 +3,7 @@ import BlockhubLogo from "./navbar/blockhub-logo";
 
 export default function Header() {
   return (
-    <header className="relative bg-orange-100 max-w-full bg-cover shadow-[0px_10px_30px_rgba(0,0,0,0.8)]">
+    <header className="relative bg-orange-100 max-w-full bg-cover"> {/*shadow-[0px_10px_30px_rgba(0,0,0,0.8)] */}
           <div className="container sticky justify-between items-center mx-auto flex z-50">
           
             {/* Logo */} 
@@ -44,20 +44,20 @@ export default function Header() {
               <div className="relative group">
             <button className="rotate-180 flex w-10 h-10 bg-[url(/assets/Profile.webp)] sketchy sketchy-none justify-center items-center bg-cover rounded-full hover:shadow-lg transition-transform duration-300 hover:scale-110 hover:rotate-0">
             </button>
-            <div className="absolute right-0 mt-2 w-40 bg-white text-neutral-700 rounded shadow-md hidden group-hover:block">
-              <a href="/profile" className="block px-4 py-2 hover:bg-gray-100">
+            <div className="absolute right-0 mt-2 w-40 bg-orange-100 rounded-3xl shadow-lg hidden group-hover:block border-2 border-neutral-700 overflow-hidden rounded-e-2xl">
+              <a href="/profile" className="block px-4 py-2 text-neutral-700 hover:text-white hover:bg-neutral-700">
                 Profile
               </a>
-              <a href="/settings" className="block px-4 py-2 hover:bg-gray-100">
+              <a href="/settings" className="block px-4 py-2 text-neutral-700 hover:text-white hover:bg-neutral-700">
                 Settings
               </a>
-              <a href="/logout" className="block px-4 py-2 hover:bg-gray-100">
+              <a href="/logout" className="block px-4 py-2 text-neutral-700 hover:text-white hover:bg-neutral-700">
                 Logout
               </a>
             </div>
               </div>
               </div>
-        </div> <div className="text-white border-2 border-neutral-700 sketchy"></div>
+        </div> {/*<div className="text-white border-2 border-neutral-700 sketchy"></div>*/}
     </header>
   );
 }
